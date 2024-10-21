86cf5dcada29506fdcf5256d539bd44cea45bb3c
Chris Hallett <chrishallettprogrammer@gmail.com>
2024-10-21 21:03:11 +1100

    fix-379 Fixing issue with default value on enums for help text

---
+++ tests/CommandLine.Tests/Fakes/Options_With_Empty_Enum_First_Element.cs
@@ -0,0 +1,37 @@
+﻿using CommandLine.Text;
+using System.Collections.Generic;
+
+namespace CommandLine.Tests.Fakes
+{
+    public enum EntityType
+    {
+        T0,
+        T1,
+        T2
+    }
+
+    public class Options_With_Empty_Enum_First_Element
+    {
+        [Option('t', "type", HelpText = "My entity")]
+        public EntityType BaseEnum { get; set; }
+
+        [Option('v', "value")]
+        public int Value { get; set; }
+        [Option('d')]
+        public double Double { get; set; }
+        [Option('b')]
+        public bool Bool { get; set; }
+
+        [Usage(ApplicationAlias = "test.exe")]
+        public static IEnumerable<Example> Examples
+        {
+            get
+            {
+                yield return new Example("1", new Options_With_Empty_Enum_First_Element { BaseEnum = EntityType.T0 });
+                yield return new Example("2", new Options_With_Empty_Enum_First_Element { BaseEnum = EntityType.T1 });
+                yield return new Example("3", new Options_With_Empty_Enum_First_Element { Value = 1, Double = 0.1, Bool = false });
+                yield return new Example("4", new Options_With_Empty_Enum_First_Element { BaseEnum = EntityType.T0, Value = 1, Double = 0.1, Bool = true });
+            }
+        }
+    }
+}
---
+++ tests/CommandLine.Tests/Unit/Issue379Tests.cs
@@ -0,0 +1,28 @@
+﻿using CommandLine.Tests.Fakes;
+using CommandLine.Text;
+using System.Linq;
+using Xunit;
+
+namespace CommandLine.Tests.Unit
+{
+    public class Issue379Tests
+    {
+        [Fact]
+        public void OptionExamples_With_Default_Values_Should_Display_In_Help_Text()
+        {
+            // Fixture setup
+            ParserResult<Options_With_Empty_Enum_First_Element> result =
+                new NotParsed<Options_With_Empty_Enum_First_Element>(
+                    TypeInfo.Create(typeof(Options_With_Empty_Enum_First_Element)), Enumerable.Empty<Error>());
+
+            // Exercize system
+            var text = HelpText.RenderUsageText(result);
+
+            // Verify outcome
+            Assert.Contains("test.exe --type T0", text);
+            Assert.Contains("test.exe --type T1", text);
+            Assert.Contains("test.exe -d 0.1 --type T0 --value 1", text);
+            Assert.Contains("test.exe -b -d 0.1 --type T0 --value 1", text);
+        }
+    }
+}
